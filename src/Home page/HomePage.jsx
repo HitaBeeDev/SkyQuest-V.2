df9d8d8f6097f1d43f9a8a26c55f4d42a9187b22
Anahita <anahita.amiri103@gmail.com>
2024-03-25 20:03:24 +0300

    home

--- src/Home page/HomePage.jsx
+++ src/Home page/HomePage.jsx
@@ -3,9 +3,14 @@ import HomeFlightSearch from "./HomeFlightSearch/HomeFlightSearch";
 
 function HomePage() {
   return (
-    <div>
-      <HomeFlightSearch />
-      <HomeCarouselSlider />
+    <div className="grid grid-cols-12 gap-5 h-screen">
+      <div className="bg-red-100 col-span-6 m-5 rounded-md shadow-xl">
+        <HomeFlightSearch />
+      </div>
+
+      <div className="bg-red-200 col-span-6 m-5 rounded-md shadow-xl">
+        <HomeCarouselSlider />
+      </div>
     </div>
   );
 }
