bc75eab7a820620033f20c23c5bcc56dd242f1fe
Anahita <anahita.amiri103@gmail.com>
2024-03-26 20:40:15 +0300

    search

--- src/Home page/HomeFlightSearch/HomeFlightSearch.jsx
+++ src/Home page/HomeFlightSearch/HomeFlightSearch.jsx
@@ -1,11 +1,13 @@
 function HomeFlightSearch() {
   return (
-    <form>
+    <form className="flex flex-col justify-between h-full p-10">
       <div>
         <label htmlFor="from">From</label>
         <input id="from" type="text" placeholder="Enter departure location" />
 
         <button type="button">Reverse</button>
+
+        <i className="fas fa-arrow-right-arrow-left"></i>
 
         <label htmlFor="to">To</label>
         <input id="to" type="text" placeholder="Enter destination location" />
