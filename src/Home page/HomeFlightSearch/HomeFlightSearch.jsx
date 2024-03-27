396a5e42504863bf7d4b37c96108a35407a14348
Anahita <anahita.amiri103@gmail.com>
2024-03-27 10:47:35 +0300

    economy

--- src/Home page/HomeFlightSearch/HomeFlightSearch.jsx
+++ src/Home page/HomeFlightSearch/HomeFlightSearch.jsx
@@ -72,17 +72,53 @@ function HomeFlightSearch() {
         </div>
       </div>
 
-      <div className="flex flex-row justify-between">
+      <div className="flex flex-row gap-5">
+        <label htmlFor="direct">Direct Flight</label>
+        <input id="direct" type="checkbox" />
+      </div>
+
+      <div className="flex flex-col w-full">
         <div className="flex flex-col">
-          <div className="flex flex-row gap-5">
-            <label htmlFor="direct">Direct Flight</label>
-            <input id="direct" type="checkbox" />
+          <div className="flex flex-row justify-between">
+            <p>Adult</p>
+
+            <div className="flex flex-row justify-between gap-3">
+              <button>-</button>
+              <p>1</p>
+              <button>+</button>
+            </div>
           </div>
 
-          <div></div>
+          <div className="flex flex-row justify-between">
+            <p>Child</p>
+
+            <div className="flex flex-row justify-between gap-3">
+              <button>-</button>
+              <p>0</p>
+              <button>+</button>
+            </div>
+          </div>
+
+          <div className="flex flex-row justify-between">
+            <p>Infant</p>
+
+            <div className="flex flex-row justify-between gap-3">
+              <button>-</button>
+              <p>0</p>
+              <button>+</button>
+            </div>
+          </div>
+
+          <div className="flex flex-row justify-between">
+            <button>ECONOMY</button>
+            <button>BUSSINESS</button>
+          </div>
         </div>
-        <button type="submit">Search</button>
       </div>
+
+      <button className="text-right" type="submit">
+        Search
+      </button>
     </form>
   );
 }
