07094f397c92ceea8028aedcfd2af73a7333c0c1
Anahita <anahita.amiri103@gmail.com>
2024-03-27 22:44:33 +0300

    commit

--- src/Home page/HomeFlightSearch/HomeFlightSearch.jsx
+++ src/Home page/HomeFlightSearch/HomeFlightSearch.jsx
@@ -72,7 +72,7 @@ function HomeFlightSearch() {
         </div>
       </div>
 
-      <div className="flex flex-row gap-5">
+      <div className="flex flex-row gap-5 items-center">
         <label htmlFor="direct">Direct Flight</label>
         <input id="direct" type="checkbox" />
       </div>
