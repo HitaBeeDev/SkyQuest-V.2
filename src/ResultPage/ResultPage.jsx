045dd279f959d5438683d7af00a8ec35750b9e07
Anahita <anahita.amiri103@gmail.com>
2024-04-11 09:43:04 +0300

    commit

---
+++ src/ResultPage/ResultPage.jsx
@@ -0,0 +1,19 @@
+function ResultPage() {
+  return (
+    <div className="bg-red-400">
+      <p>DEPARTURE</p>
+
+      <p>arrival</p>
+
+      <p>DEPARTURE date</p>
+
+      <p>arrival date</p>
+
+      <p>passanger type counts</p>
+
+      <p>passanger class</p>
+    </div>
+  );
+}
+
+export default ResultPage;
