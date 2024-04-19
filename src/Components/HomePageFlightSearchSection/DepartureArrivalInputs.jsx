945ba32320339002e8b67f8ec5746d55a6c73401
Anahita <anahita.amiri103@gmail.com>
2024-04-19 11:11:08 +0300

    commit

--- src/Components/HomePageFlightSearchSection/DepartureArrivalInputs.jsx
+++ src/Components/HomePageFlightSearchSection/DepartureArrivalInputs.jsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-
 function DepartureArrivalInputs() {
   const cities = [
     { name: "New York City" },
@@ -11,22 +9,7 @@ function DepartureArrivalInputs() {
 
   return (
     <div>
-      <input
-        type="text"
-        placeholder="Search movies..."
-        value={inputValue}
-        onChange={handleInputChange}
-      />
-
-      {open && (
-        <div>
-          {options.map((option, index) => (
-            <div key={index} onClick={() => handleOptionSelect(option)}>
-              {option.title}
-            </div>
-          ))}
-        </div>
-      )}
+      <input type="text" placeholder="Search movies..." />
     </div>
   );
 }
