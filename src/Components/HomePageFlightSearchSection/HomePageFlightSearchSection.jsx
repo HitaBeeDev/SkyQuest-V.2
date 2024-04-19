5994dc7f57b23bf840c5b2c56b836ec2ba2f31e0
Anahita <anahita.amiri103@gmail.com>
2024-04-19 14:18:51 +0300

    commit

--- src/Components/HomePageFlightSearchSection/HomePageFlightSearchSection.jsx
+++ src/Components/HomePageFlightSearchSection/HomePageFlightSearchSection.jsx
@@ -3,7 +3,7 @@ import SearchInputs from "./SearchInputs";
 function HomePageFlightSearchSection() {
   return (
     <div>
-      <div className="flex flex-row">
+      <div className="flex flex-row bg-red-200">
         <SearchInputs placeholder="Put your Departure City" />
         <SearchInputs placeholder="Put your Arrival City" />
       </div>
