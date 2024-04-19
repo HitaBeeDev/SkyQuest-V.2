452f3d75bed46d7b1352ef7e5072b1f00bbb4648
Anahita <anahita.amiri103@gmail.com>
2024-04-19 10:56:29 +0300

    commit

--- src/Components/Container.jsx
+++ src/Components/Container.jsx
@@ -1,12 +1,9 @@
+import HomePageFlightSearchSection from "./HomePageFlightSearchSection/HomePageFlightSearchSection";
+
 function Container() {
   return (
     <div>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus
-        architecto eos sapiente quia fugit eveniet adipisci natus at,
-        reprehenderit aliquid dolorem debitis, eligendi quis impedit temporibus
-        et eaque laudantium ratione.
-      </p>
+      <HomePageFlightSearchSection />
     </div>
   );
 }
