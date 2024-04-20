af5b2128e4eaad633502dd87670f0533201b2d12
Anahita <anahita.amiri103@gmail.com>
2024-04-20 15:05:47 +0300

    c

---
+++ src/Components/DateInputs/SwitchComponent.jsx
@@ -0,0 +1,31 @@
+import { alpha, styled } from "@mui/material/styles";
+import { pink } from "@mui/material/colors";
+import Switch from "@mui/material/Switch";
+
+const PinkSwitch = styled(Switch)(({ theme }) => ({
+  "& .MuiSwitch-switchBase.Mui-checked": {
+    color: pink[600],
+    "&:hover": {
+      backgroundColor: alpha(pink[600], theme.palette.action.hoverOpacity),
+    },
+  },
+  "& .MuiSwitch-switchBase.Mui-checked + .MuiSwitch-track": {
+    backgroundColor: pink[600],
+  },
+}));
+
+const label = { inputProps: { "aria-label": "Color switch demo" } };
+
+function SwitchComponent() {
+  return (
+    <div>
+      <Switch {...label} defaultChecked />
+      <Switch {...label} defaultChecked color="secondary" />
+      <Switch {...label} defaultChecked color="warning" />
+      <Switch {...label} defaultChecked color="default" />
+      <PinkSwitch {...label} defaultChecked />
+    </div>
+  );
+}
+
+export default SwitchComponent;
